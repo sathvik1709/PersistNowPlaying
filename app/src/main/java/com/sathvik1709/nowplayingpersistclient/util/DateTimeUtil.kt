cab54b2a1f83a9790ccec07dd4661ed62f546e7d
Sathvik Shivaprakash <sathvikshivaprakash519@gmail.com>
2018-05-05 10:10:08 -0700

    Added pretty time library

--- app/src/main/java/com/sathvik1709/nowplayingpersistclient/util/DateTimeUtil.kt
+++ app/src/main/java/com/sathvik1709/nowplayingpersistclient/util/DateTimeUtil.kt
@@ -1,26 +1,15 @@
 package com.sathvik1709.nowplayingpersistclient.util
 
 import org.joda.time.DateTime
-import org.joda.time.LocalDate
-import org.joda.time.format.DateTimeFormat
+import org.ocpsoft.prettytime.PrettyTime
 
 
 class DateTimeUtil {
 
-    val main_list_format = "MMM dd yyyy"
-
     fun convertToMainListFormat(time : Long) : String{
-
-        val dtf = DateTimeFormat.forPattern(main_list_format)
+        var prettyTime = PrettyTime()
         val dt = DateTime(time)
-
-        if(dt.toLocalDate().equals(LocalDate.now())){
-            return "to do" // TODO
-        }else if(dt.toLocalDate().equals(LocalDate.now().minusDays(1))){
-            return "Yesterday"
-        }
-
-        val datetime = dtf.print(dt)
-        return datetime
+        return prettyTime.format(dt.toDate())
     }
+
 }
